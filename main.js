95e03bf2d25f1e0a5a423ad11f9a81be6c010956
johan-lasso <johna-lasso@johan-pc.home>
2024-08-08 12:43:01 -0500

    feat: display current player and implement turn switching logic
    - Added functionality to display the current player on the game board
    - Implemented turn switching logic after each move
    - Updated game board to reflect player moves

--- main.js
+++ main.js
@@ -1,17 +1,50 @@
+let currentPlayer = 'X';
+
+const grid = ["","","","","","","","",""];
+
 const startBtn = document.getElementById("start-btn");
 const themeBtn = document.getElementById("theme-btn");
 const board = document.getElementById("board");
 const menu = document.getElementById("menu");
-
-let currentPlayer = 'X';
+const cells = document.querySelectorAll(".cell");
+const player = document.getElementById("current-player");
 
 startBtn.addEventListener("click", () => {
     menu.setAttribute("visible", "false"); 
+
+    // When the popout animation ends the board appears
     menu.addEventListener("animationend", () => {
         board.setAttribute("visible", "true");
         board.style.display = "flex ";
 
         menu.style.display = "none";
     });
+
+    startGame();
 });
 
+function startGame() {
+    cells.forEach(cell => cell.addEventListener("click", fillCell)); // fillCell callback
+
+    player.textContent = currentPlayer;
+}
+
+function fillCell() {
+    const desiredIndex = this.getAttribute("data-index");
+
+    // If the cell is not empty do nothing
+    if(grid[desiredIndex] != "") {
+        return;
+    }
+
+    updateCell(this, desiredIndex)
+
+    currentPlayer = (currentPlayer === 'X')? 'O' : 'X';
+
+    player.textContent = currentPlayer;
+}  
+
+function updateCell(cell, cellIndex) {
+    grid[cellIndex] = currentPlayer;
+    cell.textContent = currentPlayer;
+}   
